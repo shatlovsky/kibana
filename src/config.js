c9fc6b1ad44b61fb5dd6d68dc467979b9ebb388d
Alexey Shatlovsky <alexey.shatlovsky@booking.com>
2014-01-20 10:25:26 +0100

    kibana-dev: Fixed ES url

--- src/config.js
+++ src/config.js
@@ -21,7 +21,7 @@ function (Settings) {
      * kibana installed on. You probably want to set it to the FQDN of your
      * elasticsearch host
      */
-    elasticsearch: "http://"+window.location.hostname+":9200",
+    elasticsearch: "/elasticsearch",
 
     /** @scratch /configuration/config.js/5
      * ==== default_route
